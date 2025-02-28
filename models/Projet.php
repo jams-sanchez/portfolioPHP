7e4f7bc354ce20b4008bf351894b157e0a5e05ba
James Sanchez <james.sanchez@laplateforme.io>
2025-02-28 18:04:41 +0100

    Ajout de nouvelles méthodes pour récupérer, modifier et supprimer des projets, leurs techs et leurs images.

--- models/Projet.php
+++ models/Projet.php
@@ -21,8 +21,46 @@ class Projet extends Bdd
         $this->projectLink = $projectLink;
     }
 
+    // methode pour recuperer un projet par id
+    public function getProjetById($idProjet)
+    {
+        $stmt = "SELECT projet.id, projet.nom, projet.description, 
+        projet.lien, projet.image_id,
+        image.bin AS image,
+        tech.nom AS tech
+        FROM projet
+        JOIN projet_tech ON projet_tech.projet_id = projet.id
+        JOIN tech ON projet_tech.tech_id = tech.id
+        JOIN image ON projet.image_id = image.id
+        WHERE projet.id = :projetId";
+        $stmt = $this->bdd->prepare($stmt);
+        $stmt->execute([
+            ':projetId' => $idProjet
+        ]);
+        $stmt = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $projet = [];
+        foreach ($stmt as $key => $value) {
+
+            if (empty($projet)) {
+                $projet = [
+                    'id' => $value['id'],
+                    'nom' => $value['nom'],
+                    'desc' => $value['description'],
+                    'lien' => $value['lien'],
+                    'imageId' => $value['image_id'],
+                    'image' => $value['image'],
+                    'techs' => []
+                ];
+            }
+
+            $projet['techs'][] = $value['tech'];
+        }
+        return $projet;
+    }
+
     // méthode pour afficher tous les projets 
-    public function getAllProjets()
+    public function getAllProjets(): array
     {
         $stmt = "SELECT projet.id, projet.nom, projet.description, 
         projet.lien, projet.image_id,
@@ -59,7 +97,7 @@ class Projet extends Bdd
     }
 
     // méthode pour ajouter un projet
-    public function insertProject($nom, $description, $lien, $techs, $image)
+    public function insertProject($nom, $description, $lien, $techs, $image): void
     {
         $addProjectStmt = "INSERT INTO projet(nom, description, lien, image_id)
         VALUES (:nom, :description, :lien, :image_id)";
@@ -84,8 +122,50 @@ class Projet extends Bdd
             ]);
         }
     }
+
+    // méthode pour modifier un projet 
+    public function updateProjet($id, $nom, $desc, $lien): void
+    {
+        $updateStmt = "UPDATE projet 
+        SET nom = :nom, description = :desc, lien = :lien
+        WHERE projet.id = :id";
+        $updateStmt = $this->bdd->prepare($updateStmt);
+        $updateStmt->execute([
+            ':id' => $id,
+            ':nom' => $nom,
+            ':desc' => $desc,
+            ':lien' => $lien
+        ]);
+    }
+
+    // ---------------------------------------------------------- //
+    // méthode pour supprimer l'association de tech à un projet
+    public function deleteProjetTech($projetId): void
+    {
+        $projectTechStmt = "DELETE FROM projet_tech
+        WHERE projet_id = :projet_id";
+        $projectTechStmt = $this->bdd->prepare($projectTechStmt);
+        $projectTechStmt->execute([
+            ':projet_id' => $projetId
+        ]);
+    }
+
+    public function insertProjetTech($projetId, $techs)
+    {
+        $projectTechStmt = "INSERT INTO projet_tech (projet_id, tech_id)
+        VALUES (:projet_id, :tech_id)";
+        $projectTechStmt = $this->bdd->prepare($projectTechStmt);
+
+        foreach ($techs as $tech) {
+            $projectTechStmt->execute([
+                ':projet_id' => $projetId,
+                ':tech_id' => $tech
+            ]);
+        }
+    }
 }
 
 // $projet = new Projet();
+// $selectedProjet = $projet->getProjetById(9);
 
-// var_dump($projet->getAllProjets());
+// echo $selectedProjet['nom'];
--- pages/admin-projets.php
+++ pages/admin-projets.php
@@ -62,18 +62,124 @@ if (isset($_POST['validProjet'])) {
 
             if ($image->addImage($imageName, $imageType, $imageSize, $imageBin)) {
 
-                // ajout de tech info + lie image
+                // ajout des infos du projet + lie image
                 $imageId = $image->getLastImageId();
                 $projet->insertProject($projetName, $projetDesc, $projetLink, $projetTech, $imageId);
 
                 $_SESSION['succes'] = "Succès - Projet ajouté";
                 $_POST['addProjet'] = "+";
                 header('refresh: 1; url=../pages/admin-projets.php?page=projets');
+                exit();
             }
         }
     }
 }
 
+// MODIFIER PROJET
+// recuperation du projet a modifier
+if (isset($_POST['updateProjet'])) {
+    $idProjet = $_POST['updateProjet'];
+    $_SESSION['projetId'] = $idProjet;
+
+    $selectedProjet = $projet->getProjetById($idProjet);
+
+    $_SESSION['projetName'] = $selectedProjet['nom'];
+    $_SESSION['projetDesc'] = $selectedProjet['desc'];
+    $_SESSION['projetLien'] = $selectedProjet['lien'];
+    $_SESSION['projetTechs'] = $selectedProjet['techs'];
+}
+
+// modification projet et son image
+if (isset($_POST['validUpdateP'])) {
+    $idProjet = $_SESSION['projetId'];
+
+    // modification infos du projet
+    if (!empty($_POST['updateName'])) {
+        $projetName = htmlspecialchars($_POST['updateName']);
+    } else {
+        $projetName = $_SESSION['projetName'];
+    }
+    if (!empty($_POST['updateDesc'])) {
+        $projetDesc = htmlspecialchars($_POST['updateDesc']);
+    } else {
+        $projetDesc = $_SESSION['projetDesc'];
+    }
+    if (!empty($_POST['updateLink'])) {
+        $projetLink = htmlspecialchars($_POST['updateLink']);
+    } else {
+        $projetLink = $_SESSION['projetLien'];
+    }
+
+    $projet->updateProjet($idProjet, $projetName, $projetDesc, $projetLink);
+
+    // modification des techs du projet
+    if (!empty($_POST['updateTech'])) {
+        $projetTech = $_POST['updateTech'];
+
+        if ($projetTech != $_SESSION['projetTechs']) {
+            $projet->deleteProjetTech($idProjet);
+            $projet->insertProjetTech($idProjet, $projetTech);
+        }
+    } else {
+        $projetTech = $_SESSION['projetTechs'];
+    }
+
+
+    // modification image
+    if (!empty($_FILES['updateImage'])) {
+        $imageName = $_FILES['updateImage']['name'];
+        $imageType = $_FILES['updateImage']['type'];
+        $imageSize = $_FILES['updateImage']['size'];
+        $imageBin = $_FILES['updateImage']['tmp_name'];
+        $imageError = $_FILES['updateImage']['error'];
+
+        // vérifie les erreurs d'upload
+        if ($imageError !== UPLOAD_ERR_OK) {
+            $_SESSION['erreur'] = "Image non modifié.";
+        } elseif ($imageSize > 5000000) {
+            $_SESSION['erreur'] = "La taille de l'image dépasse la limite autorisée de 5MB.";
+        } elseif (!in_array($imageType, ['image/jpeg', 'image/png', 'image/webp'])) {
+            $_SESSION['erreur'] = "Le type de fichier n'est pas autorisé. Seuls les fichiers JPEG, PNG et WEBP sont acceptés.";
+        } else {
+            $image->updateImageTech($idProjet, $imageName, $imageType, $imageSize, $imageBin);
+        }
+    }
+
+    $_SESSION['succes'] = "Succès - Projet mis à jour";
+    unset($_SESSION['projetId']);
+    unset($_SESSION['projetName']);
+    unset($_SESSION['projetDesc']);
+    unset($_SESSION['projetLien']);
+    unset($_SESSION['projetTechs']);
+    header('refresh: 1; url=../pages/admin-projets.php?page=projets');
+    exit();
+}
+
+if (isset($_POST['cancelProjetP'])) {
+    unset($_SESSION['projetId']);
+    unset($_SESSION['projetName']);
+    unset($_SESSION['projetDesc']);
+    unset($_SESSION['projetLien']);
+    unset($_SESSION['projetTechs']);
+    unset($_POST);
+    header('Location: ../pages/admin-projets.php?page=projets');
+    exit();
+}
+
+// SUPPRIMER UN PROJET
+if (isset($_POST['deleteProjet'])) {
+
+    $projetId = $_POST['deleteProjet'];
+
+    // supprime les techs associé au projet
+    $projet->deleteProjetTech($projetId);
+
+    // supprimer l'image de la tech supprime la tech associé
+    $image->deleteImageProjet($projetId);
+    $_SESSION['succes'] = "Succès - Projet supprimé";
+    header('refresh: 1; url=../pages/admin-projets.php?page=projets');
+    exit();
+}
 
 
 ?>
@@ -96,7 +202,7 @@ if (isset($_POST['validProjet'])) {
         <?php endif; ?>
 
         <!-- ajout de projet -->
-        <?php if (isset($_POST['addProjet'])) : ?>
+        <?php if (isset($_POST['addProjet'])): ?>
 
             <form action="" method="post" enctype="multipart/form-data" class="box-projet">
                 <!-- add nom -->
@@ -107,12 +213,14 @@ if (isset($_POST['validProjet'])) {
                 <!-- add description -->
                 <div class="projet-input">
                     <label for="newDesc" class="bold">Description : </label>
-                    <textarea name="newDesc" id="newDesc" class="input-text" placeholder="décris le projet" rows="5" cols="38" require></textarea>
+                    <textarea name="newDesc" id="newDesc" class="input-text" placeholder="décris le projet" rows="5" cols="38"
+                        require></textarea>
                 </div>
                 <!-- add lien -->
                 <div class="projet-input">
                     <label for="newLink" class="bold">Lien Github : </label>
-                    <input type="text" name="newLink" id="newLink" class="input-text" placeholder="www.github.com/jams-sanchez/" require>
+                    <input type="text" name="newLink" id="newLink" class="input-text" placeholder="www.github.com/jams-sanchez/"
+                        require>
                 </div>
                 <!-- add tech -->
                 <div class="projet-input">
@@ -139,6 +247,61 @@ if (isset($_POST['validProjet'])) {
 
             </form>
 
+            <!-- modifie un projet -->
+        <?php elseif (isset($_POST['updateProjet'])): ?>
+
+            <form action="" method="post" enctype="multipart/form-data" class="box-projet">
+                <!-- update nom -->
+                <div class="projet-input">
+                    <label for="updateProjet" class="bold">Nom : </label>
+                    <input type="text" name="updateName" id="updateName" class="input-text"
+                        placeholder="<?= $_SESSION['projetName']; ?>">
+                </div>
+                <!-- update description -->
+                <div class="projet-input">
+                    <label for="updateDesc" class="bold">Description : </label>
+                    <textarea name="updateDesc" id="updateDesc" class="input-text" placeholder="<?= $_SESSION['projetDesc']; ?>"
+                        rows="5" cols="38"></textarea>
+                </div>
+                <!-- update lien -->
+                <div class="projet-input">
+                    <label for="updateLink" class="bold">Lien Github : </label>
+                    <input type="text" name="updateLink" id="updateLink" class="input-text"
+                        placeholder="<?= $_SESSION['projetLien']; ?>">
+                </div>
+                <!-- update tech -->
+                <div class="projet-input">
+                    <p class="bold">Tech : </p>
+                    <div class="projet-input-tech">
+                        <?php foreach ($listTech as $id => $nom): ?>
+                            <?php if (in_array($nom, $_SESSION['projetTechs'])): ?>
+                                <div class="tech-box">
+                                    <label for="updateTech"><?= $nom ?></label>
+                                    <input type="checkbox" name="updateTech[]" id="updateTech" value="<?= $id ?>" checked>
+                                </div>
+                            <?php else: ?>
+                                <div class="tech-box">
+                                    <label for="updateTech"><?= $nom ?></label>
+                                    <input type="checkbox" name="updateTech[]" id="updateTech" value="<?= $id ?>">
+                                </div>
+                            <?php endif; ?>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+                <!-- update logo -->
+                <div class="projet-input">
+                    <label for="updateImage" class="bold">Image : </label>
+                    <input type="file" name="updateImage" id="updateImage" class="input-file" accept=".jpg,.jpeg,.webp,.png"
+                        require>
+                </div>
+                <div class="duo-button">
+                    <input type="submit" name="cancelProjetP" class="button-red" value="Annuler">
+                    <input type="submit" name="validUpdateP" class="button-green" value="Valider">
+
+                </div>
+
+            </form>
+
         <?php else: ?>
 
             <!-- affiche toutes les techs -->
@@ -148,7 +311,7 @@ if (isset($_POST['validProjet'])) {
             </form>
 
             <section class="show-projet">
-                <?php foreach ($listProjet as $projet => $info) : ?>
+                <?php foreach ($listProjet as $projet => $info): ?>
                     <div class="box-projet">
                         <div class="box-projet-info">
                             <div class="projet-img">
@@ -173,8 +336,8 @@ if (isset($_POST['validProjet'])) {
                             </section>
                         </div>
                         <form action="" method="post" class="duo-button">
-                            <button type="submit" name="deleteTech" class="button-red" value="<?= $projet ?>">Supprimer</button>
-                            <button type="submit" name="updateTech" class="button-yellow" value="<?= $projet ?>">Modifier</button>
+                            <button type="submit" name="deleteProjet" class="button-red" value="<?= $projet ?>">Supprimer</button>
+                            <button type="submit" name="updateProjet" class="button-yellow" value="<?= $projet ?>">Modifier</button>
                         </form>
                     </div>
                 <?php endforeach; ?>
